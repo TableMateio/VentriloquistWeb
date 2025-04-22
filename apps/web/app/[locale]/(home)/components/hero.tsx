e439ff42c250cbb73d326f9106665a488e6cfca4
TableMateio <scott@millennialtarot.com>
2025-04-22 16:17:15 -0400

    Restore 3D effect for hero image

--- apps/web/app/[locale]/(home)/components/hero.tsx
+++ apps/web/app/[locale]/(home)/components/hero.tsx
@@ -74,12 +74,21 @@ export const Hero = async ({ dictionary }: HeroProps) => {
 
           {/* Right Side - Image */}
           <div className="md:w-1/2 md:pl-8 flex justify-center">
-            <div className="relative w-[600px] h-[550px]">
-              <img 
-                src="/assets/hero.png" 
-                alt="Ventriloquist Node Interface" 
-                className="w-full h-full object-contain"
-              />
+            <div className="relative w-[600px] h-[550px] transform perspective-1000 -mt-12">
+              <div className="absolute inset-0 bg-white/10 rounded-xl shadow-2xl transform -rotate-3 scale-110"></div>
+              <div 
+                className="relative w-full h-full rounded-xl overflow-hidden border-8 border-white/20 shadow-[0_15px_30px_rgba(0,0,0,0.4)] transform rotate-3 transition-transform hover:rotate-0 duration-500"
+                style={{ 
+                  transformStyle: 'preserve-3d',
+                  transform: 'perspective(1000px) rotateY(-8deg) rotateX(3deg)'
+                }}
+              >
+                <img 
+                  src="/assets/hero.png" 
+                  alt="Ventriloquist Node Interface" 
+                  className="w-full h-full object-contain bg-gradient-to-b from-[#1F5C3D]/5 to-[#2E8B57]/10 p-4"
+                />
+              </div>
             </div>
           </div>
 
